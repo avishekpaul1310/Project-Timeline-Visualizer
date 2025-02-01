d3483e5485d755d03d70e2b81eeba0fc8aaef803
avishekpaul1310 <avishekpaul1310@gmail.com>
2025-02-01 18:59:01 +0530

    Add proper .gitignore file

--- timeline_app/templatetags/form_filters.py
+++
@@ -1,7 +0,0 @@
-from django import template
-
-register = template.Library()
-
-@register.filter(name='addclass')
-def addclass(field, css_class):
-    return field.as_widget(attrs={'class': css_class})
