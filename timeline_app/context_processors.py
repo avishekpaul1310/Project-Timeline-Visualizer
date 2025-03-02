e2985d67c2eacb8ed2cc57d3507456bf7a5fde1b
Avishekpaul1310 <avishekpaul1310@gmail.com>
2025-03-02 08:41:19 +0530

    commit

--- timeline_app/context_processors.py
+++ timeline_app/context_processors.py
@@ -1,8 +1,8 @@
-# timeline_app/context_processors.py
 def notifications_processor(request):
     unread_notifications_count = 0
     if request.user.is_authenticated:
         unread_notifications_count = request.user.notifications.filter(is_read=False).count()
+        print(f"User {request.user.username} has {unread_notifications_count} unread notifications")
     
     return {
         'unread_notifications_count': unread_notifications_count
