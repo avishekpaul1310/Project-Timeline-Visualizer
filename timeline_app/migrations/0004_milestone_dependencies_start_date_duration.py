6fd77f07313eb2a2787e05c26054e988cd7bfb4d
Avishekpaul1310 <avishekpaul1310@gmail.com>
2025-03-27 10:25:50 +0530

    Add dependencies, start_date, and duration fields to Milestone model

---
+++ timeline_app/migrations/0004_milestone_dependencies_start_date_duration.py
@@ -0,0 +1,27 @@
+# Generated manually
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('timeline_app', '0003_alter_milestone_status'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='milestone',
+            name='dependencies',
+            field=models.ManyToManyField(blank=True, related_name='dependent_milestones', to='timeline_app.milestone'),
+        ),
+        migrations.AddField(
+            model_name='milestone',
+            name='start_date',
+            field=models.DateField(blank=True, null=True),
+        ),
+        migrations.AddField(
+            model_name='milestone',
+            name='duration',
+            field=models.PositiveIntegerField(default=1, help_text='Duration in days'),
+        ),
